67006d725d84c1f22f8e8fea41617c7adad1f0fc
lichin-lin <vic20087cjimlin.cs03@nctu.edu.tw>
2018-04-26 14:02:46 +0800

    end state position

--- src/containers/game/EndState.js
+++ src/containers/game/EndState.js
@@ -15,6 +15,19 @@ class EndState extends Phaser.State {
     console.log(this.scoreBoard)
   }
   async create () {
+    // your score
+    let result = { score: this.score, time: moment().format('YYYY-MM-DD hh:mm:ss') }
+    try {
+      const cookies = new Cookies()
+      let user = cookies.get('user')
+      if (user) {
+        let res = await firebase.database().ref(`/user/${user.data.displayName}`).push().set(result)
+        console.log(res)
+      }
+    } catch(err) {
+      console.log(err)
+    }
+
     // all user score
     let scoreList = await firebase.database().ref(`/user/`).once('value').then((snapshot) => {
       return snapshot.val()
@@ -33,7 +46,7 @@ class EndState extends Phaser.State {
     bg.width = this.world.width
     bg.height = this.world.height
 
-    let title = this.add.text(this.world.centerX, this.world.height * 0.25, '遊戲結束', {
+    let title = this.add.text(this.world.centerX, this.world.height * 0.5, '遊戲結束', {
         fontSize: '40px',
         fontWeight: 'bold',
         fill: '#f2bb15'
@@ -41,22 +54,29 @@ class EndState extends Phaser.State {
     title.anchor.setTo(0.5, 0.5)
 
     let scoreStr = '你的得分是：' + this.score + '分'
-    let scoreText = this.add.text(this.world.centerX, this.world.height * 0.4, scoreStr, {
+    let scoreText = this.add.text(this.world.centerX, this.world.height * 0.6, scoreStr, {
         fontSize: '30px',
         fontWeight: 'bold',
         fill: '#f2bb15'
     })
     scoreText.anchor.setTo(0.5, 0.5)
 
-    let remind = this.add.text(this.world.centerX, this.world.height * 0.45, '點擊螢幕以分享', {
+    let remind = this.add.text(this.world.centerX, this.world.height * 0.65, '點擊螢幕以分享', {
         fontSize: '20px',
         fontWeight: 'bold',
         fill: '#f2bb15'
     })
     remind.anchor.setTo(0.5, 0.5)
 
+    let scoreBoardTitle = this.add.text(this.world.centerX, this.world.height * 0.1, '排行榜', {
+        fontSize: '30px',
+        fontWeight: 'bold',
+        fill: '#f2bb15'
+    })
+    scoreBoardTitle.anchor.setTo(0.5, 0.5)
+
     this.scoreBoard.map((data, i) => {
-      let record = this.add.text(this.world.centerX, this.world.height * 0.6, `${i+1}. ${data.score} - ${data.name}`, {
+      let record = this.add.text(this.world.centerX, this.world.height * 0.15, `${i+1}. ${data.score} - ${data.name}`, {
           fontSize: '16px',
           fontWeight: 'bold',
           fill: '#f2bb15'
@@ -64,17 +84,6 @@ class EndState extends Phaser.State {
       record.anchor.setTo(0.5, 0.5 - i)
     })
 
-    let result = { score: this.score, time: moment().format('YYYY-MM-DD hh:mm:ss') }
-    try {
-      const cookies = new Cookies()
-      let user = cookies.get('user')
-      if (user) {
-        let res = await firebase.database().ref(`/user/${user.data.displayName}`).push().set(result)
-        console.log(res)
-      }
-    } catch(err) {
-      console.log(err)
-    }
     // onInputUp
     this.input.onTap.add(() => {
       let url = 'https://2018shupradmooju.surge.sh'
